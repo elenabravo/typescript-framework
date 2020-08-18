6e209c8cb024d84f73a8a37a9b697bcc9ad67108
Elena Bravo <hey@elenabravo.com>
2020-08-18 20:30:25 +0200

    refractoring with composition

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { User } from './models/user'
-
-const user = new User({ name: 'new record', age: 0 })
-
-user.save()
+import { User } from './models/User'
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing'
 
 const SERVER_URL =  'http://localhost:3000'
 
@@ -8,10 +9,8 @@ interface UserProps {
     age?: number
 }
 
-type Callback = () => void
-
 export class User {
-    events: { [key: string]: Callback[] } = {}
+    private events: Eventing = new Eventing()
 
     constructor(private data: UserProps) {}
     
@@ -21,20 +20,6 @@ export class User {
 
     set(update: UserProps): void {
         Object.assign(this.data, update)
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || []
-        handlers.push(callback)
-        this.events[eventName] = handlers
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName] 
-        
-        if (!handlers || !handlers?.length) return
-
-        handlers.forEach(callback => { callback() })
     }
 
     fetch(): void {
